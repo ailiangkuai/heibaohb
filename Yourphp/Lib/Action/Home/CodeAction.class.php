87706bd446204d9faee233d4b5de9c4fb551e79d
ai凉快 <ailiangkuai@qq.com>
2016-11-26 11:52:35 +0800

    修复查询次数bug

--- Yourphp/Lib/Action/Home/CodeAction.class.php
+++ Yourphp/Lib/Action/Home/CodeAction.class.php
@@ -14,8 +14,33 @@ class CodeAction extends BaseAction
 {
 
 	function _initialize()
-    {	
-		parent::_initialize();
+    {
+        $this->sysConfig = F('sys.config');
+
+        if(APP_LANG){
+            $this->Lang = F('Lang');
+            $this->assign('Lang',$this->Lang);
+            if(get_safe_replace($_GET['l'])){
+                if(!$this->Lang[$_GET['l']]['status'])$this->error ( L ( 'NO_LANG' ) );
+                $lang=$_GET['l'];
+            }else{
+                $lang=$this->sysConfig['DEFAULT_LANG'];
+            }
+            define('LANG_NAME', $lang);
+            define('LANG_ID', $this->Lang[$lang]['id']);
+            $this->categorys = F('Category_'.$lang);
+            $this->Config = F('Config_'.$lang);
+            $this->assign('l',$lang);
+            $this->assign('langid',LANG_ID);
+            $T = F('config_'.$lang,'', APP_PATH.'Tpl/Home/'.$this->sysConfig['DEFAULT_THEME'].'/');
+            C('TMPL_CACHFILE_SUFFIX','_'.$lang.'.php');
+            cookie('think_language',$lang);
+        }else{
+            $T = F('config_'.$this->sysConfig['DEFAULT_LANG'],'',  APP_PATH.'Tpl/Home/'.$this->sysConfig['DEFAULT_THEME'].'/');
+            $this->categorys = F('Category');
+            $this->Config = F('Config');
+            cookie('think_language',$this->sysConfig['DEFAULT_LANG']);
+        }
     }
 
     public function index()
